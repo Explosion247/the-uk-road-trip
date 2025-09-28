7ce0c664d42489bfcd908e00429772df15f7e1b3
Explosion247 <Dave.staward@gmail.com>
2025-09-28 19:47:11 +0100

    began the README page, including the Site Overview, table of content and some information with the colours

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// // Google Maps API keys
+// const API_KEY =
+//   "773d429319c7860a2cf86fdcb96e47fc7a12df4cc7c10ba302b8dc5f218064af";
+// const API_URL =
+//   "https://serpapi.com/search.json?engine=google_maps&q=Coffee&ll=@40.7455096,-74.0083012,14z";
+
+// const { getJson } = require("serpapi");
+
+// getJson(
+//   {
+//     engine: "google_maps",
+//     q: "Coffee",
+//     ll: "@40.7455096,-74.0083012,14z",
+//     api_key: "773d429319c7860a2cf86fdcb96e47fc7a12df4cc7c10ba302b8dc5f218064af",
+//   },
+//   (json) => {
+//     console.log(json);
+//   }
+// );
