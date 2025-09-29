653dec58bf053b38f777a789cec266726ba84be1
Explosion247 <Dave.staward@gmail.com>
2025-09-29 20:28:19 +0100

    added Text search to the google maps, moved the main html into index.html and cleared up some unused files like index2 and script.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,75 +1,74 @@
-// Initialize and add the map
-// let map;
+const API_KEY = "AIzaSyBzaDXE_CFHiVY5BYuoEZbJ6ZZVYRj7vnc";
+let map;
+let markers = {};
+let infoWindow;
 
-// async function initMap() {
-//   // The location of Uluru
-//   const position = { lat: -25.344, lng: 131.031 };
-//   // Request needed libraries.
-//   //@ts-ignore
-//   const { Map } = await google.maps.importLibrary("maps");
-//   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-//   // The map, centered at Uluru
-//   map = new Map(document.getElementById("map"), {
-//     zoom: 4,
-//     center: position,
-//     mapId: "DEMO_MAP_ID",
-//   });
-
-//   // The marker, positioned at Uluru
-//   const marker = new AdvancedMarkerElement({
-//     map: map,
-//     position: position,
-//     title: "Uluru",
-//   });
-// }
-
-// initMap();
-
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see the error "The Geolocation service
-// failed.", it means you probably did not give permission for the browser to
-// locate you.
-let map, infoWindow;
-
-function initMap() {
+// initiate the map
+async function initMap() {
+  // Request needed libraries.
+  const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  const myLatlng = { lat: -25.363, lng: 131.044 };
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 6,
+    zoom: 8,
+    center: myLatlng,
+    mapId: "DEMO_MAP_ID",
   });
-  infoWindow = new google.maps.InfoWindow();
 
-  const locationButton = document.createElement("button");
+  // const marker = new google.maps.marker.AdvancedMarkerElement({
+  //   position: myLatlng,
+  //   map,
+  //   title: "Click to zoom",
+  // });
+  // sets the location to the users current location
+  let infoWindow = new google.maps.InfoWindow();
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        infoWindow.setPosition(pos);
+        infoWindow.setContent("Your current Location");
+        infoWindow.open(map);
+        map.setCenter(pos);
+      },
+      () => {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
+  // map.addListener("center_changed", () => {
+  //   // 3 seconds after the center of the map has changed, pan back to the
+  //   // marker.
+  //   window.setTimeout(() => {
+  //     map.panTo(marker.position);
+  //   }, 3000);
+  // });
+  // marker.addListener("click", () => {
+  //   map.setZoom(8);
+  //   map.setCenter(marker.position);
+  // });
 
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
-          infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
-        }
-      );
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
+  const textInput = document.getElementById("text-input");
+  const textInputButton = document.getElementById("text-input-button");
+  const card = document.getElementById("text-input-card");
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(card);
+  textInputButton.addEventListener("click", () => {
+    findPlaces(textInput.value);
+  });
+  textInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      findPlaces(textInput.value);
     }
   });
 }
-
+// function to handle no Geolocation
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
   infoWindow.setContent(
@@ -79,5 +78,60 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   );
   infoWindow.open(map);
 }
+async function findPlaces(query) {
+  const { Place } = await google.maps.importLibrary("places");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  const request = {
+    textQuery: query,
+    fields: ["displayName", "location", "businessStatus"],
+    includedType: "",
+    locationBias: { lat: -25.363, lng: 131.044 },
+    isOpenNow: true,
+    language: "en-GB",
+    maxResultCount: 8,
+    minRating: 3.2,
+    region: "GB",
+    useStrictTypeFiltering: false,
+  };
+  //@ts-ignore
+  const { places } = await Place.searchByText(request);
 
-window.initMap = initMap;
+  if (places.length) {
+    console.log(places);
+
+    const { LatLngBounds } = await google.maps.importLibrary("core");
+    const bounds = new LatLngBounds();
+
+    // Loop through and get all the results.
+    places.forEach((place) => {
+      const marker = new AdvancedMarkerElement({
+        map,
+        position: place.location,
+        title: place.displayName,
+      });
+      markers[place.id] = marker;
+      marker.addListener("gmp-click", () => {
+        map.panTo(place.location);
+        updateInfoWindow(place.displayName, place.id, marker);
+      });
+      if (place.location != null) {
+        bounds.extend(place.location);
+        console.log(place);
+      }
+    });
+    console.log(`bounds ${bounds}`);
+    map.fitBounds(bounds);
+  } else {
+    console.log("No results");
+  }
+}
+async function updateInfoWindow(title, content, anchor) {
+  infoWindow.setContent(content);
+  infoWindow.setHeaderContent(title);
+  infoWindow.open({
+    map,
+    anchor,
+    shouldFocus: false,
+  });
+}
+initMap();
